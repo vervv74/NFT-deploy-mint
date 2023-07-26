bb6d4cbc1837e21ad9a8dafb93fa6641878b0c13
QWE <vervv74@gmail.com>
2023-07-26 17:18:31 +0300

    2

---
+++ CV_NFT.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.8.3/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.8.3/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.8.3/access/Ownable.sol";
+import "@openzeppelin/contracts@4.8.3/utils/Counters.sol";
+
+contract MyToken1 is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("MyToken1", "MTK1") {}
+
+    function publicMint(string memory uri) public payable {
+        require(msg.value == 0.01 ether, "Not Enough Funds");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
